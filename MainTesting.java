f2e2aae505973ce528af453c5344f214ce365cc1
prasanthilab <prasanthi.sarvasudhi@gmail.com>
2024-08-10 15:23:40 +0530

    fourth commit

--- MainTesting.java
+++ MainTesting.java
@@ -1,7 +1,6 @@
 
-
 public class MainTesting {
-public static void main(String[] args) {
-	System.out.println("Hii testing main class");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello how are u?");
+	}
 }
